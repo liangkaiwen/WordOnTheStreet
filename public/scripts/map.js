39637307a07d05e14d047579a9deaaab4f8df740
liangkaiwen <kevinl95@yahoo.com>
2015-10-18 02:52:14 -0700

    added current location handling

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,7 +1,24 @@
+//initializes Google Map
 var initMap = function() {
-	var map = new google.maps.Map(document.getElementById("map"), {
+	//getLocation();
+	var map = new google.maps.Map($("#map")[0], {
     	center: {lat: -34.397, lng: 150.644},
     	scrollwheel: false,
     	zoom: 8
-  });
+  	});
+
+  	if (navigator.geolocation) {
+  		navigator.geolocation.getCurrentPosition(function(position) {
+  			var pos = {
+        		lat: position.coords.latitude,
+       		 	lng: position.coords.longitude
+      		};
+      		map.setCenter(pos);
+  		}, function() {
+  			console.log("GPS location access denied");
+  		});
+  	} else {
+  		//Device does not support Geolocation
+  		console.log("Device not supported");
+  	}
 }
