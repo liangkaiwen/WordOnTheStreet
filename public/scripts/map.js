7f3ae443639d3f4c268a5c20cc4ac02ee6697bd3
5ives <kevinl95@cs.washington.edu>
2015-10-17 23:47:24 -0700

    set up index.html

---
+++ public/scripts/map.js
@@ -0,0 +1,7 @@
+var renderMap = function() {
+	var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    scrollwheel: false,
+    zoom: 8
+  });
+}
