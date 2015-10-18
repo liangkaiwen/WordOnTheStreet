ba367740fe39f94d90d23308a8208c861aeb786d
liangkaiwen <kevinl95@yahoo.com>
2015-10-18 04:39:47 -0700

    resolved merge conflict

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,7 +1,33 @@
-var renderMap = function() {
-	var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    scrollwheel: false,
-    zoom: 8
-  });
+//initializes Google Map
+var initMap = function() {
+	var map = new google.maps.Map($("#map")[0], {
+    	scrollwheel: false,
+    	zoom: 1
+  	});
+
+  	if (navigator.geolocation) {
+  		navigator.geolocation.getCurrentPosition(function(position) {
+  			var pos = {
+        		lat: position.coords.latitude,
+       		 	lng: position.coords.longitude
+      		};
+      		map.setCenter(pos);
+
+      		var userMarker = new google.maps.Marker({
+      			position: pos,
+      			map: map,
+      			draggable: false,
+      			animation: google.maps.Animation.DROP,
+      			title: "We are here"
+      		});
+
+      		map.setZoom(13);
+
+  		}, function() {
+  			console.log("GPS location access denied");
+  		});
+  	} else {
+  		//Device does not support Geolocation
+  		console.log("Device not supported");
+  	}
 }
